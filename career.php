e2196f045ce8c3693203815a9798d43b2c8babd3
zenakrua <zenakrua@gmail.com>
2018-12-03 21:40:40 -0500

    Finally got json to parse

--- career.php
+++ career.php
@@ -9,24 +9,18 @@
 
 <?php
 
-$key = "c01f6d205597419db64dd68c";
+	$key = "c01f6d205597419db64dd68c";
 
-$jsonData = file_get_contents("https://xivapi.com/Item/2?columns=LevelItem,ItemUICategory.Name,Name&key=".$key);
+	$jsonData = file_get_contents("https://xivapi.com/Item?columns=LevelItem,ItemUICategory.Name,Name&key=".$key);
 
-$items = json_decode($jsonData, true);
+	$items = json_decode($jsonData, true);
 
-var_dump($items);
+	foreach($items["Results"] as $item){
 
-//foreach($items->data as $item){
-	
-	//echo $item;
+		echo $item["Name"];
 
-	//$image_url = 'https://storage.cloud.google.com/manasmith-221002.appspot.com/images/adventurers/icons/'.$adventurer['id'].'_'.$adventurer['variation'].'.png';
-	
-	//echo '<table class="adventurer '.$adventurer['element'].' rank'.$adventurer['newrank'].' collected'.$adventurer['collected'].'"><tr class="header"><th>'.$adventurer['name'].'</th></tr><tr class="icon"><td style="background: url(\''.$image_url.'\') no-repeat center"></td></tr></table>';
+	}
 
-//}
-	
 ?>
 
 </section>
