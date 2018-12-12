1aa672627d56073e588a81b73740306d4d9324a9
zenakrua <zenakrua@msn.com>
2018-12-12 14:23:53 -0500

    Update career.php

--- career.php
+++ career.php
@@ -8,36 +8,33 @@
 		
 			if (this.readyState == 4 && this.status == 200) {
 		
-				document.getElementsByTagName("list").innerHTML = this.statusText;
+				document.getElementById("list").innerHTML = this.responseText;
 		
 			}
+     
 		};
 		
-		xhttp.open("GET", "https://xivapi.com/search?indexes=Recipe&filters=ClassJob.ID=9,RecipeLevelTable.ClassJobLevel>=1,RecipeLevelTable.ClassJobLevel<=5&key=c01f6d205597419db64dd68c", true);
+		xhttp.open("GET", "https://xivapi.com/search?key=c01f6d205597419db64dd68c&indexes=Recipe&filters=ClassJob.ID=8,RecipeLevelTable.ClassJobLevel>=1,RecipeLevelTable.ClassJobLevel<=5", true);
 		xhttp.send();
 		
 	}
 		
-	function getMaterials() {
-	
-	}
-	
 </script>
 
-<log>
+<div id="log">
 
-	<nav>
+	<div id="nav">
 		
-		<ul id="classes">Class
+		<ul id="jobs">Jobs
 			
-			<li><button type="button" class="class" value="8" onclick="getRecipes()">Carpenter</button></li>
-			<li><button type="button" class="class" value="9" onclick="getRecipes()">Blacksmith</button></li>
-			<li><button type="button" class="class" value="10" onclick="getRecipes()">Armorer</button></li>
-			<li><button type="button" class="class" value="11" onclick="getRecipes()">Goldsmith</button></li>
-			<li><button type="button" class="class" value="12" onclick="getRecipes()">Leatherworker</button></li>
-			<li><button type="button" class="class" value="13" onclick="getRecipes()">Weaver</button></li>
-			<li><button type="button" class="class" value="14" onclick="getRecipes()">Alchemist</button></li>
-			<li><button type="button" class="class" value="15" onclick="getRecipes()">Culinarian</button></li>
+			<li><button type="button" class="job" value="8" onclick="getRecipes()">Carpenter</button></li>
+			<li><button type="button" class="job" value="9" onclick="getRecipes()">Blacksmith</button></li>
+			<li><button type="button" class="job" value="10" onclick="getRecipes()">Armorer</button></li>
+			<li><button type="button" class="job" value="11" onclick="getRecipes()">Goldsmith</button></li>
+			<li><button type="button" class="job" value="12" onclick="getRecipes()">Leatherworker</button></li>
+			<li><button type="button" class="job" value="13" onclick="getRecipes()">Weaver</button></li>
+			<li><button type="button" class="job" value="14" onclick="getRecipes()">Alchemist</button></li>
+			<li><button type="button" class="job" value="15" onclick="getRecipes()">Culinarian</button></li>
 		
 		</ul>
 		
@@ -47,33 +44,14 @@
 		
 		</ul>
 	
-	</nav>
+	</div>
 
-	<list>
+	<div id="list">
 
-		<?php
+	</div>
 
-//			$endpoint = "https://xivapi.com/";
-//			$key = "key=c01f6d205597419db64dd68c";
+	<div id="recipe">
 
-//			$jsonData = file_get_contents($endpoint."search?indexes=Item&filters=ItemUICategory.Name_en=Crystal&".$key);
-
-//			$items = json_decode($jsonData, true);
-
-//			foreach($items["Results"] as $item){
-
-//				echo "<table><tr><td>".$item["Name"]."</td></tr></table>";
-
-//			}
-
-		?>
-
-	</list>
-
-	<recipe>
-
-		test
-
-	</recipe>
+	</div>
 	
-</log>
+</div>
