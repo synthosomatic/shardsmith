ff362a36ea790b165136092fa37d4c02f3faf39f
zenakrua <zenakrua@msn.com>
2018-12-12 16:04:40 -0500

    Update career.php

--- career.php
+++ career.php
@@ -1,52 +1,88 @@
 <script>
 	
-	function getRecipes(val) {
+	function getJob(val) {
 		
-        	var job = val;
-		var xhttp = new XMLHttpRequest();
-        
-        	xhttp.onreadystatechange = function() {
+		var job = val;
 		
-			if (this.readyState == 4 && this.status == 200) {
-		
-				document.getElementById("list").innerHTML = this.responseText;
-		
-			} else {
-            
-            			document.getElementById("list").innerHTML = this.statusText;
-            
-			}
-     
-		};
-		
-		xhttp.open("GET", "https://xivapi.com/search?key=c01f6d205597419db64dd68c&indexes=Recipe&filters=ClassJob.ID="+job+",RecipeLevelTable.ClassJobLevel>=1,RecipeLevelTable.ClassJobLevel<=5", true);
-		xhttp.send();
+		var levelRange = document.createElement("li");
+		var levelRangeButton = document.createElement("button");
+		var levelRangeLabel = document.createTextNode("1-5");
+
+		// Get the <ul> element with id="recipeLevel"
+		var list = document.getElementById("recipeLevel");
+
+		// As long as <ul> has a child node, remove it
+		while (list.hasChildNodes()) {   
+		list.removeChild(list.firstChild);
+		}
+		levelRange.appendChild(levelRangeButton);
+		levelRangeButton.appendChild(levelRangeLabel);
+		document.getElementById("recipeLevel").appendChild(levelRange);
+			
+		function getRecipes(val) {
+
+			var xhttp = new XMLHttpRequest();
+
+			xhttp.onreadystatechange = function() {
+
+				if (this.readyState == 4 && this.status == 200) {
+
+					document.getElementById("list").innerHTML = this.responseText;
+
+				} else {
+
+					document.getElementById("list").innerHTML = this.statusText;
+
+				}
+
+			};
+
+			xhttp.open("GET", "https://xivapi.com/search?key=c01f6d205597419db64dd68c&indexes=Recipe&filters=ClassJob.ID="+job+",RecipeLevelTable.ClassJobLevel>=1,RecipeLevelTable.ClassJobLevel<=5", true);
+			xhttp.send();
+
+		}
 		
 	}
 		
 </script>
 
+<div id="nav">
+		
+	<ul id="jobs">Crafting Log
+
+		<li><button type="button" class="job" value="8" onclick="getJob(this.value)">Carpenter</button></li>
+		<li><button type="button" class="job" value="9" onclick="getJob(this.value)">Blacksmith</button></li>
+		<li><button type="button" class="job" value="10" onclick="getJob(this.value)">Armorer</button></li>
+		<li><button type="button" class="job" value="11" onclick="getJob(this.value)">Goldsmith</button></li>
+		<li><button type="button" class="job" value="12" onclick="getJob(this.value)">Leatherworker</button></li>
+		<li><button type="button" class="job" value="13" onclick="getJob(this.value)">Weaver</button></li>
+		<li><button type="button" class="job" value="14" onclick="getJob(this.value)">Alchemist</button></li>
+		<li><button type="button" class="job" value="15" onclick="getJob(this.value)">Culinarian</button></li>
+
+	</ul>
+
+</div>
+
 <div id="log">
 
 	<div id="nav">
 		
-		<ul id="jobs">Jobs
-			
-			<li><button type="button" class="job" value="8" onclick="getRecipes(this.value)">Carpenter</button></li>
-			<li><button type="button" class="job" value="9" onclick="getRecipes(this.value)">Blacksmith</button></li>
-			<li><button type="button" class="job" value="10" onclick="getRecipes(this.value)">Armorer</button></li>
-			<li><button type="button" class="job" value="11" onclick="getRecipes(this.value)">Goldsmith</button></li>
-			<li><button type="button" class="job" value="12" onclick="getRecipes(this.value)">Leatherworker</button></li>
-			<li><button type="button" class="job" value="13" onclick="getRecipes(this.value)">Weaver</button></li>
-			<li><button type="button" class="job" value="14" onclick="getRecipes(this.value)">Alchemist</button></li>
-			<li><button type="button" class="job" value="15" onclick="getRecipes(this.value)">Culinarian</button></li>
-		
-		</ul>
+		<p>Recipe Search</p>
 		
 		<ul>Extras
 			
 			<li>Favorites</li>
 		
+		</ul>
+		
+		<p>Recipe Level</p>
+		
+		<ul id="levelRange">
+		
+			<button>Recipes</button><button>Special</button>
+
+			<li><button type="button" class="job" value="8" onclick="getRecipes(this.value)">1-5</button></li>
+			
 		</ul>
 	
 	</div>
