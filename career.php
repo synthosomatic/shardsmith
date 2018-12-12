a8c1eb8046a2da2340d4c69551f9b5e43de7bec4
zenakrua <zenakrua@msn.com>
2018-12-12 14:41:28 -0500

    Update career.php

--- career.php
+++ career.php
@@ -1,20 +1,25 @@
 <script>
 	
-	function getRecipes() {
+	function getRecipes(val) {
 		
+        	var job = val;
 		var xhttp = new XMLHttpRequest();
-		
-		xhttp.onreadystatechange = function() {
+        
+        	xhttp.onreadystatechange = function() {
 		
 			if (this.readyState == 4 && this.status == 200) {
 		
 				document.getElementById("list").innerHTML = this.responseText;
 		
+			} else {
+            
+            			document.getElementById("list").innerHTML = this.statusText;
+            
 			}
      
 		};
 		
-		xhttp.open("GET", "https://xivapi.com/search?key=c01f6d205597419db64dd68c&indexes=Recipe&filters=ClassJob.ID=8,RecipeLevelTable.ClassJobLevel>=1,RecipeLevelTable.ClassJobLevel<=5", true);
+		xhttp.open("GET", "https://xivapi.com/search?key=c01f6d205597419db64dd68c&indexes=Recipe&filters=ClassJob.ID="+job+",RecipeLevelTable.ClassJobLevel>=1,RecipeLevelTable.ClassJobLevel<=5", true);
 		xhttp.send();
 		
 	}
@@ -27,14 +32,14 @@
 		
 		<ul id="jobs">Jobs
 			
-			<li><button type="button" class="job" value="8" onclick="getRecipes()">Carpenter</button></li>
-			<li><button type="button" class="job" value="9" onclick="getRecipes()">Blacksmith</button></li>
-			<li><button type="button" class="job" value="10" onclick="getRecipes()">Armorer</button></li>
-			<li><button type="button" class="job" value="11" onclick="getRecipes()">Goldsmith</button></li>
-			<li><button type="button" class="job" value="12" onclick="getRecipes()">Leatherworker</button></li>
-			<li><button type="button" class="job" value="13" onclick="getRecipes()">Weaver</button></li>
-			<li><button type="button" class="job" value="14" onclick="getRecipes()">Alchemist</button></li>
-			<li><button type="button" class="job" value="15" onclick="getRecipes()">Culinarian</button></li>
+			<li><button type="button" class="job" value="8" onclick="getRecipes(this.value)">Carpenter</button></li>
+			<li><button type="button" class="job" value="9" onclick="getRecipes(this.value)">Blacksmith</button></li>
+			<li><button type="button" class="job" value="10" onclick="getRecipes(this.value)">Armorer</button></li>
+			<li><button type="button" class="job" value="11" onclick="getRecipes(this.value)">Goldsmith</button></li>
+			<li><button type="button" class="job" value="12" onclick="getRecipes(this.value)">Leatherworker</button></li>
+			<li><button type="button" class="job" value="13" onclick="getRecipes(this.value)">Weaver</button></li>
+			<li><button type="button" class="job" value="14" onclick="getRecipes(this.value)">Alchemist</button></li>
+			<li><button type="button" class="job" value="15" onclick="getRecipes(this.value)">Culinarian</button></li>
 		
 		</ul>
 		
