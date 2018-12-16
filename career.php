52014ee404bd8e13dcc979a2d9a4d66d9e07cb6c
Sarah Schaller <zenakrua@gmail.com>
2018-12-15 23:41:52 -0500

    Updating

--- career.php
+++ career.php
@@ -2,130 +2,138 @@
 	
 // Get data from API
 function fetchData(url, cFunction) {
-  var xhttp;
-  xhttp = new XMLHttpRequest();
-  xhttp.responseType = "json";
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      cFunction(this);
-    }
-  };
-  xhttp.open("GET", url, true);
-  xhttp.send();
+    var xhttp;
+    xhttp = new XMLHttpRequest();
+    xhttp.responseType = "json";
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cFunction(this);
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
 }
 
 // Get level IDs from RecipeLevelTable
 function getLevels(xhttp) {
-	// Get the <ul> element with id="recipeLevel"
-  var recipeLevels = document.getElementById("recipeLevel");
-  // As long as <ul> has a child node, remove it
-  while (recipeLevels.hasChildNodes()) {
-    recipeLevels.removeChild(recipeLevels.firstChild);
-  }
+    // Get the <ul> element with id="recipeLevel"
+    var recipeLevels = document.getElementById("recipeLevel");
+    // As long as <ul> has a child node, remove it
+    while (recipeLevels.hasChildNodes()) {
+        recipeLevels.removeChild(recipeLevels.firstChild);
+    }
 
-  var rawData = xhttp.response;
-  var data = [];
-	var levels = [];
+    var rawData = xhttp.response;
+    var data = [];
+    var levels = [];
 
 
-  // Extract the level IDs into their own array
-  for (i in rawData.Results) {
-	
-    var level = rawData.Results[i]['ID'];
-    data.push(level);
+    // Extract the level IDs into their own array
+    for (i in rawData.Results) {
 
-  }
-	
-	levels = data.filter(function(value, index, Arr) {
-	
-		return index % 5 == 0;
-		
-	});
-	
-	var levelRanges = "";
-	
-	for (i = 0; i < levels.length; i++) {
-	
-		var lower = levels[i];
-		var upper = levels[i] + 4;
+        var level = rawData.Results[i]['ID'];
+        data.push(level);
 
-		document.getElementById("recipeLevel").innerHTML = levelRanges += "<li><button type=\"button\" class=\"levelRange\" onclick=\"fetchData('https://xivapi.com/search?indexes=Recipe&filters=ClassJob.ID=" + job + ",ItemResult.ItemSearchCategoryTargetID>0,RecipeLevelTable.ClassJobLevel>=" + lower + ",RecipeLevelTable.ClassJobLevel<=" + upper + "', getRecipes)\">" + lower + "-" + upper + "</button></li>" + "<br />";
-			
-	}
-	
+    }
+
+    levels = data.filter(function(value, index, Arr) {
+
+        return index % 5 == 0;
+
+    });
+
+    var levelRanges = "";
+
+    for (i = 0; i < levels.length; i++) {
+
+        var lower = levels[i];
+        var upper = levels[i] + 4;
+
+        document.getElementById("recipeLevel").innerHTML = levelRanges += "<li><button type=\"button\" class=\"levelRange\" onclick=\"fetchData('https://xivapi.com/search?indexes=Recipe&filters=ClassJob.ID=" + job + ",ItemResult.ItemSearchCategoryTargetID>0,RecipeLevelTable.ClassJobLevel>=" + lower + ",RecipeLevelTable.ClassJobLevel<=" + upper + "', getRecipes)\">" + lower + "-" + upper + "</button></li>" + "<br />";
+
+    }
+
 }
 
 // Get recipes for specified job
 function getRecipes(xhttp) {
-	// Get the <ul> element with id="recipeLevel"
-  var recipeList = document.getElementById("recipeList");
-  // As long as <ul> has a child node, remove it
-  while (recipeList.hasChildNodes()) {
-    recipeList.removeChild(recipeList.firstChild);
-  }
+    // Get the <ul> element with id="recipeLevel"
+    var recipeList = document.getElementById("recipeList");
+    // As long as <ul> has a child node, remove it
+    while (recipeList.hasChildNodes()) {
+        recipeList.removeChild(recipeList.firstChild);
+    }
 
-  var rawData = xhttp.response;
+    var rawData = xhttp.response;
 
-  var list = "";
-	
-	for (i = 0; i < rawData.Results.length; i++) {
-	
-		var id = rawData.Results[i]['ID'];
-		var name = rawData.Results[i]['Name'];
-		
-		document.getElementById("recipeList").innerHTML = list += "<li><button type=\"button\" class=\"recipe\" onclick=\"fetchData('https://xivapi.com/recipe/" + id + "', getRecipeMaterials)\">" + name + "</button></li>" + "<br />";
-			
-	}
-	
+    var list = "";
+
+    for (i = 0; i < rawData.Results.length; i++) {
+
+        var id = rawData.Results[i]['ID'];
+        var name = rawData.Results[i]['Name'];
+
+        document.getElementById("recipeList").innerHTML = list += "<li><button type=\"button\" class=\"recipe\" onclick=\"fetchData('https://xivapi.com/recipe/" + id + "', getRecipeMaterials)\">" + name + "</button></li>" + "<br />";
+
+    }
+
 }
+
 
 </script>
 
-<div id="nav">
+<div id="craftingLog">
 
-  <ul id="jobs">Crafting Log
+    <div id="jobList">
 
-    <li><button type="button" class="job" value="8" onclick="job=this.value;fetchData('https://xivapi.com/RecipeLevelTable?columns=ID', getLevels)">Carpenter</button></li>
-    <li><button type="button" class="job" value="9" onclick="job=this.value;fetchData('https://xivapi.com/RecipeLevelTable?columns=ID', getLevels)">Blacksmith</button></li>
-    <li><button type="button" class="job" value="10" onclick="job=this.value;fetchData('https://xivapi.com/RecipeLevelTable?columns=ID', getLevels)">Armorer</button></li>
-    <li><button type="button" class="job" value="11" onclick="job=this.value;fetchData('https://xivapi.com/RecipeLevelTable?columns=ID', getLevels)">Goldsmith</button></li>
-    <li><button type="button" class="job" value="12" onclick="job=this.value;fetchData('https://xivapi.com/RecipeLevelTable?columns=ID', getLevels)">Leatherworker</button></li>
-    <li><button type="button" class="job" value="13" onclick="job=this.value;fetchData('https://xivapi.com/RecipeLevelTable?columns=ID', getLevels)">Weaver</button></li>
-    <li><button type="button" class="job" value="14" onclick="job=this.value;fetchData('https://xivapi.com/RecipeLevelTable?columns=ID', getLevels)">Alchemist</button></li>
-    <li><button type="button" class="job" value="15" onclick="job=this.value;fetchData('https://xivapi.com/RecipeLevelTable?columns=ID', getLevels)">Culinarian</button></li>
+        <p><span>Crafting Log</span><span>Level 51 Miner</span></p>
 
-  </ul>
+        <ul id="jobs">
 
-</div>
+            <li><button type="button" class="job" value="8" onclick="job=this.value;fetchData('https://xivapi.com/RecipeLevelTable?columns=ID', getLevels)"><img src="https://storage.googleapis.com/shardsmith-210018.appspot.com/images/icons/icon_job_Carpenter.png"/></button></li>
+            <li><button type="button" class="job" value="9" onclick="job=this.value;fetchData('https://xivapi.com/RecipeLevelTable?columns=ID', getLevels)"><img src="https://storage.googleapis.com/shardsmith-210018.appspot.com/images/icons/icon_job_Blacksmith.png"/></button></li>
+            <li><button type="button" class="job" value="10" onclick="job=this.value;fetchData('https://xivapi.com/RecipeLevelTable?columns=ID', getLevels)"><img src="https://storage.googleapis.com/shardsmith-210018.appspot.com/images/icons/icon_job_Armorer.png"/></button></li>
+            <li><button type="button" class="job" value="11" onclick="job=this.value;fetchData('https://xivapi.com/RecipeLevelTable?columns=ID', getLevels)"><img src="https://storage.googleapis.com/shardsmith-210018.appspot.com/images/icons/icon_job_Goldsmith.png"/></button></li>
+            <li><button type="button" class="job" value="12" onclick="job=this.value;fetchData('https://xivapi.com/RecipeLevelTable?columns=ID', getLevels)"><img src="https://storage.googleapis.com/shardsmith-210018.appspot.com/images/icons/icon_job_Leatherworker.png"/></button></li>
+            <li><button type="button" class="job" value="13" onclick="job=this.value;fetchData('https://xivapi.com/RecipeLevelTable?columns=ID', getLevels)"><img src="https://storage.googleapis.com/shardsmith-210018.appspot.com/images/icons/icon_job_Weaver.png"/></button></li>
+            <li><button type="button" class="job" value="14" onclick="job=this.value;fetchData('https://xivapi.com/RecipeLevelTable?columns=ID', getLevels)"><img src="https://storage.googleapis.com/shardsmith-210018.appspot.com/images/icons/icon_job_Alchemist.png"/></button></li>
+            <li><button type="button" class="job" value="15" onclick="job=this.value;fetchData('https://xivapi.com/RecipeLevelTable?columns=ID', getLevels)"><img src="https://storage.googleapis.com/shardsmith-210018.appspot.com/images/icons/icon_job_Culinarian.png"/></button></li>
+			<li><button type="button" class="job" value="15">Search</button></li>
+			
+        </ul>
+		
+	</div>
 
-<div id="log">
+    <div id="log">
 
-  <div id="nav">
+        <div id="logMenu">
 
-    <p>Recipe Search</p>
+            <p>Recipe Search</p>
 
-    <ul>Extras
+            <ul>Extras
 
-      <li>Favorites</li>
+                <li>Favorites</li>
 
-    </ul>
+            </ul>
 
-    <p>Recipe Level</p>
+            <p>Recipe Level</p>
 
-    <button>Recipes</button><button>Special</button>
+            <button>Recipes</button><button>Special</button>
 
-    <ul id="recipeLevel">
+            <ul id="recipeLevel">
 
-    </ul>
+            </ul>
 
-  </div>
+        </div>
 
-  <div id="recipeList">
+        <div id="recipeList">
 
-  </div>
+        </div>
 
-  <div id="recipeMats">
+        <div id="recipeMats">
 
-  </div>
+        </div>
+
+    </div>
 
 </div>
