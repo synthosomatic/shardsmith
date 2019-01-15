6ea931612c70335a07792b41125fe36ce2d6f6fa
zenakrua <zenakrua@msn.com>
2019-01-14 19:59:31 -0500

    Update header.php

--- header.php
+++ header.php
@@ -18,35 +18,34 @@
 </head>
 <body>
 <header>
-	<nav>
-		<svg width="26.5625em" height="2.5em">
-			<defs>
-				<linearGradient id="backing" x1="0%" y1="0%" x2="0%" y2="100%">
-					<stop offset="0%" style="stop-color:#475787;stop-opacity:.15" />
-					<stop offset="100%" style="stop-color:#b4a678;stop-opacity:.15" />
-				</linearGradient>
-				<linearGradient id="text" x1="0%" y1="0%" x2="0%" y2="100%">
-					<stop offset="0%" style="stop-color:#fff;stop-opacity:1" />
-					<stop offset="100%" style="stop-color:#876b1f;stop-opacity:1" />
-				</linearGradient>
-			</defs>
-			<a href="/">
-				<text text-anchor="middle" dominant-baseline="central" fill="url(#backing)" font-family="Eorzea" x="50%" y="50%" font-size="4.0625em">SHARDSMITH</text>
-				<text id="logo" text-anchor="middle" dominant-baseline="central" fill="url(#text)" font-family="Open Sans" font-weight="300" x="50%" y="50%" font-size="1.5em" letter-spacing="1em">SHARDSMITH</text>
-			</a>
-		</svg>
-		<ul>
-			<li>
-				<a href="/">Home</a>
-			</li>
-			<li>
-				<a href="/career/">Career</a>
-			</li>
+	<svg id="logo" width="26.5625em" height="2.5em">
+		<defs>
+			<linearGradient id="backing" x1="0%" y1="0%" x2="0%" y2="100%">
+				<stop offset="0%" style="stop-color:#475787;stop-opacity:.15" />
+				<stop offset="100%" style="stop-color:#b4a678;stop-opacity:.15" />
+			</linearGradient>
+			<linearGradient id="text" x1="0%" y1="0%" x2="0%" y2="100%">
+				<stop offset="0%" style="stop-color:#fff;stop-opacity:1" />
+				<stop offset="100%" style="stop-color:#876b1f;stop-opacity:1" />
+			</linearGradient>
+		</defs>
+		<a href="/">
+			<text id="backing" text-anchor="middle" dominant-baseline="central" fill="url(#backing)" font-family="Eorzea" x="50%" y="50%" font-size="4.0625em">SHARDSMITH</text>
+			<text id="text" text-anchor="middle" dominant-baseline="central" fill="url(#text)" font-family="Open Sans" font-weight="300" x="50%" y="50%" font-size="1.5em" letter-spacing="1em">SHARDSMITH</text>
+		</a>
+	</svg>
+	<ul>
+		<li>
+			<a href="/">Home</a>
+		</li>
+		<li>
+			<a href="/career/">Career</a>
+		</li>
 <!--			<li>
-				<a href="/join/">Create an Account</a>
-			</li>
-			<li>
-				<?php
+			<a href="/join/">Create an Account</a>
+		</li>
+		<li>
+			<?php
 //					if (isset($_POST["username"])) {
 //						echo "Welcome, ", $_POST["username"], "! <a href=\"logout.php\">Logout</a>?";
 //					} else {
@@ -56,10 +55,11 @@
 //							<button type=\"submit\" value=\"submit\" >Login</button>
 //						</form>";
 //					};
-				?>
-			</li>-->
-		</ul>
+			?>
+		</li>-->
+	</ul>
+	<classToggle>
 		<img src="https://storage.googleapis.com/shardsmith-210018.appspot.com/images/icons/icon_job_Dragoon.png" />
-	</nav>
+	</classToggle>
 </header>
 <main>
