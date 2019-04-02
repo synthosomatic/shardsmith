552fce6d1b4cf316e4f4e188afbf044e70bb347e
zenakrua <zenakrua@msn.com>
2019-04-01 20:01:19 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ if ($_SERVER['REQUEST_URI']=='/') {
 	include 'home.php';
 } elseif ($_SERVER['REQUEST_URI']=='/career/') {
 	include 'career.php';
+} elseif ($_SERVER['REQUEST_URI']=='/quests/') {
+	include 'quests.php';
 } elseif ($_SERVER['REQUEST_URI']=='/join/') {
 	include 'join.php';
 } elseif ($_SERVER['REQUEST_URI']=='/test/') {
