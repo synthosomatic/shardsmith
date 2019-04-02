ccd31089cbf1de9dee5c30ab72a2b575f5faa63c
zenakrua <zenakrua@msn.com>
2019-04-02 15:45:21 -0400

    Update quests.php

--- quests.php
+++ quests.php
@@ -1,21 +1,23 @@
 <script>
   
-function getQuests(xhttp) {
-	var quests = "";
+function getJournalSections(xhttp) {
+	
+	var journalSections = "";
 	var rawData = xhttp.response;
-	var questlist = document.getElementsByTagName("quests")[0];
 	
 	for (i = 0; i < rawData.Results.length; i++) {
-		var id = rawData.Results[i]['ID'];
+		
 		var name = rawData.Results[i]['Name'];
 		
-		questlist.innerHTML = quests += "<p>" + id + " " + name + "</p>";
+		document.getElementsByTagName("journalSection").getElementByID("span").innerHTML = journalSections += "<button>" + name + "</button>";
+	
 	}
+
 }
   
 </script>
   
-<quests onload="fetchData('https://xivapi.com/Quest?key=c01f6d205597419db64dd68c', getQuests)">Quests</quests>
+<button onclick="fetchData('https://xivapi.com/JournalSection?key=c01f6d205597419db64dd68c', getJournalSections)">Quests</button>
 
 <journal>
   
@@ -30,18 +32,7 @@ function getQuests(xhttp) {
     
     <journalSection>
       
-      <span>
-        
-        <button>MSQ (ARR/HW)</button>
-        <button>MSQ (SB)</button>
-        <button>CoaNE</button>
-        <button>Sidequests</button>
-        <button>Beast Tribe (ARR/HW)</button>
-        <button>Beast Tribe (SB)</button>
-        <button>Class/Job</button>
-        <button>Other</button>
-        
-      </span>
+      <span></span>
       
       <search></search>
       
