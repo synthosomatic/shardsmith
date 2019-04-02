1cb66700a2342d30bf460ae5f1e9aeb3a8ee6a4c
zenakrua <zenakrua@msn.com>
2019-04-02 13:41:34 -0400

    Update quests.php

--- quests.php
+++ quests.php
@@ -1,3 +1,3 @@
-<button onclick="fetchData('https://xivapi.com/Quest?key=c01f6d205597419db64dd68c&columns=ID,Name', getQuests)">Quests</button>
+<button onclick="fetchData('https://xivapi.com/Quest?key=c01f6d205597419db64dd68c', getQuests)">Quests</button>
 
 <quests></quests>
