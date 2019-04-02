91abde7fd793c6f02c1076ee8f94ff5f953a1cff
zenakrua <zenakrua@msn.com>
2019-04-02 15:39:26 -0400

    Update quests.php

--- quests.php
+++ quests.php
@@ -1,3 +1,74 @@
-<button onclick="fetchData('https://xivapi.com/Quest?key=c01f6d205597419db64dd68c', getQuests)">Quests</button>
+<script>
+  
+function getQuests(xhttp) {
+	var quests = "";
+	var rawData = xhttp.response;
+	var questlist = document.getElementsByTagName("quests")[0];
+	
+	for (i = 0; i < rawData.Results.length; i++) {
+		var id = rawData.Results[i]['ID'];
+		var name = rawData.Results[i]['Name'];
+		
+		questlist.innerHTML = quests += "<p>" + id + " " + name + "</p>";
+	}
+}
+  
+</script>
+  
+<quests onload="fetchData('https://xivapi.com/Quest?key=c01f6d205597419db64dd68c', getQuests)">Quests</quests>
 
-<quests></quests>
+<journal>
+  
+  <journalHeader>
+    
+    <title></title>
+    <subtitle></subtitle> <completedCount></completedCount>
+    
+  </journalHeader>
+  
+  <journalNav>
+    
+    <journalSection>
+      
+      <span>
+        
+        <button>MSQ (ARR/HW)</button>
+        <button>MSQ (SB)</button>
+        <button>CoaNE</button>
+        <button>Sidequests</button>
+        <button>Beast Tribe (ARR/HW)</button>
+        <button>Beast Tribe (SB)</button>
+        <button>Class/Job</button>
+        <button>Other</button>
+        
+      </span>
+      
+      <search></search>
+      
+    </journalSection>
+    
+    <journalCategory></journalCategory>
+    
+  </journalNav>
+  
+  <journalBody>
+    
+    <list>
+
+      <journalGenre>
+
+        <quests></quests>
+
+      </journalGenre>
+
+    </list>
+    
+  </journalBody>
+  
+  <journalFooter>
+    
+    <subCompletedCount></subCompletedCount>
+    
+  </journalFooter>
+  
+</journal>
