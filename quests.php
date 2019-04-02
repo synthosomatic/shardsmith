2c922c8098773348e6bf054a8b4369a3937ba4a7
zenakrua <zenakrua@msn.com>
2019-04-02 13:24:34 -0400

    Update quests.php

--- quests.php
+++ quests.php
@@ -26,4 +26,4 @@ function getQuests(xhttp) {
 }
 </script>
 	
-<quests></quests>
+<quests onload="fetchData('https://xivapi.com/Quest?key=c01f6d205597419db64dd68c&columns=ID,Name', getQuests)"></quests>
